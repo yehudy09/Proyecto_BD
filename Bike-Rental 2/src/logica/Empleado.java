1076ce240cf59842c3c5b6461e825e7a5e5d580a
Darianny Escoboza <Darianny Escoboza@DESKTOP-H6TG0VV>
2020-11-23 17:43:44 -0400

    Empleado

--- Bike-Rental 2/src/logica/Empleado.java
+++ Bike-Rental 2/src/logica/Empleado.java
@@ -1,11 +1,39 @@
 package logica;
 
 public class Empleado extends Persona {
+	
+	private String ssn;
+	private String posicion;
+	private float salario; 
 
 	public Empleado(int id, String cedula, String fname, String sname, String lname, String calle, String ciudad,
 			String tel, int postalCode) {
 		super(id, cedula, fname, sname, lname, calle, ciudad, tel, postalCode);
 		// TODO Auto-generated constructor stub
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	public String getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(String posicion) {
+		this.posicion = posicion;
+	}
+
+	public float getSalario() {
+		return salario;
+	}
+
+	public void setSalario(float salario) {
+		this.salario = salario;
 	}
 
 }

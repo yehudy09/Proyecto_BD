c12b50a328569e93b3f4e351ee8968d18e352a1a
Ezequiel Luna <Ezequiel Luna@Ezequiel-PC>
2020-11-23 16:19:58 -0400

    clases

---
+++ Bike-Rental 2/src/logica/Venta.java
@@ -0,0 +1,77 @@
+package logica;
+
+import java.sql.Date;
+
+public class Venta {
+	private int vid; 
+	private int cantVenta; 
+	private Date fechaSalida; 
+	private int descuento;  
+	private Factura factura;  
+	private Producto producto;
+	
+	public Venta(int vid, int cantVenta, Date fechaSalida, int descuento, Factura factura, Producto producto) {
+		super();
+		this.vid = vid; 
+		this.cantVenta = cantVenta;
+		this.fechaSalida = fechaSalida;
+		this.descuento = descuento;
+		this.factura = factura;
+		this.producto = producto;
+	}
+
+	
+	public int getVid() {
+		return vid;
+	}
+
+	public void setVid(int vid) {
+		this.vid = vid;
+	}
+
+
+	public int getCantVenta() {
+		return cantVenta;
+	}
+
+	public void setCantVenta(int cantVenta) {
+		this.cantVenta = cantVenta;
+	}
+
+	public Date getFechaSalida() {
+		return fechaSalida;
+	}
+
+	public void setFechaSalida(Date fechaSalida) {
+		this.fechaSalida = fechaSalida;
+	}
+
+	public int getDescuento() {
+		return descuento;
+	}
+
+	public void setDescuento(int descuento) {
+		this.descuento = descuento;
+	}
+
+	public Factura getFactura() {
+		return factura;
+	}
+
+	public void setFactura(Factura factura) {
+		this.factura = factura;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	} 
+	
+	
+	
+	
+
+}

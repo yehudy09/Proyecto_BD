5222cc86819e0dbb06e17257302897987755bad2
Yehudy <Yehudy@DESKTOP-Q5G1B41>
2020-11-25 00:59:15 -0400

    Marca

--- Bike-Rental 2/src/logica/Proveedor.java
+++ Bike-Rental 2/src/logica/Proveedor.java
@@ -1,11 +1,21 @@
 package logica;
 
 public class Proveedor extends Persona {
+	
+	private String marca;
 
 	public Proveedor(String cedula, String fname, String sname, String lname, String calle, String ciudad,
-			String tel, int postalCode) {
+			String tel, int postalCode, String marca) {
 		super(cedula, fname, sname, lname, calle, ciudad, tel, postalCode);
 		// TODO Auto-generated constructor stub
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
 	}
 
 }

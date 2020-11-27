633de6e58fdd778c9320ec0d282672f3126d6662
Yehudy <Yehudy@DESKTOP-Q5G1B41>
2020-11-27 17:17:07 -0400

    modificar empleado

--- Bike-Rental 2/src/visual/ModificarEmpleado.java
+++ Bike-Rental 2/src/visual/ModificarEmpleado.java
@@ -18,7 +18,6 @@ import logica.Empleado;
 
 
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JTextField;
 import javax.swing.JButton;
@@ -31,8 +30,6 @@ import java.text.ParseException;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowStateListener;
-
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.JComboBox;
@@ -43,11 +40,11 @@ import javax.swing.SwingConstants;
 import javax.swing.JFormattedTextField;
 import java.awt.Component;
 import java.awt.SystemColor;
+import java.awt.event.WindowStateListener;
 
-public class tablaEmpleados extends JFrame {
+public class ModificarEmpleado extends JFrame {
 
 	private JPanel contentPane;
-	private JTable tableEmpleado;
 	private JTextField tfSSN;
 	private JTextField tfNombre;
 	private JTextField tfCiudad;
@@ -61,6 +58,8 @@ public class tablaEmpleados extends JFrame {
 	private JFormattedTextField TelefonoJF;
 	private JFormattedTextField cedulaJF;
 	private JFormattedTextField CodPostJF;
+	private boolean confirmar = false;
+//	private JComboBox cbxProvincia;
 
 	/**
 	 * Launch the application.
@@ -81,28 +80,41 @@ public class tablaEmpleados extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public tablaEmpleados() {
-		addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowOpened(WindowEvent e) {
-					try {
-					cargarEmpleados();
-					} catch (Exception e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
+	public ModificarEmpleado(Empleado emp) {
+		addWindowStateListener(new WindowStateListener() {
+			public void windowStateChanged(WindowEvent e) {
 				
 			}
 		});
-		setTitle("EMPLEADOS");
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowOpened(WindowEvent e) {
+				tfSSN.setText(emp.getSsn());
+				TelefonoJF.setText(emp.getTel());
+				cedulaJF.setText(emp.getCedula());
+				CodPostJF.setText(emp.getPostalCode());
+				tfNombre.setText(emp.getFname());
+				tfSname.setText(emp.getSname());
+				tfApellido.setText(emp.getLname());
+				tfCalle.setText(emp.getCalle());
+				tfCiudad.setText(emp.getCiudad());
+				tfPosicion.setText(emp.getPosicion());
+				//cbxProvincia.setSelectedItem(emp.getProvincia());
+				tfSalario.setText(Float.toString(emp.getSalario()));
+				confirmar = true;
+			}
+			@Override
+			public void windowClosed(WindowEvent e) {
+				confirmar = true;
+			}
+		});
+		setTitle("Modificar Empleado");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1004, 609);
+		setBounds(100, 100, 1004, 273);
 		contentPane = new JPanel();
 		contentPane.setBackground(SystemColor.inactiveCaptionBorder);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		
-		JScrollPane scrollPaneEmpleado = new JScrollPane();
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(SystemColor.inactiveCaptionBorder);
@@ -176,6 +188,7 @@ public class tablaEmpleados extends JFrame {
 		JComboBox cbxProvincia = new JComboBox();
 		cbxProvincia.setBounds(603, 36, 123, 20);
 		cbxProvincia.setModel(new DefaultComboBoxModel(new String[] {"<Seleccione>", "Azua ", "Bahoruco ", "Barahona ", "Dajab\u00F3n ", "Distrito Nacional ", "Duarte ", "El\u00EDas Pi\u00F1a ", "El Seibo ", "Espaillat ", "Hato Mayor ", "Independencia ", "La Altagracia ", "La Romana ", "La Vega ", "Mar\u00EDa Trinidad S\u00E1nchez ", "Monse\u00F1or Nouel ", "Montecristi ", "Monte Plata ", "Pedernales ", "Peravia ", "Puerto Plata ", "Hermanas Mirabal ", "Saman\u00E1 ", "S\u00E1nchez Ram\u00EDrez ", "San Crist\u00F3bal ", "San Jos\u00E9 de Ocoa ", "San Juan ", "San Pedro de Macor\u00EDs ", "Santiago ", "Santiago Rodr\u00EDguez ", "Santo Domingo ", "Valverde "}));
+		cbxProvincia.setSelectedItem(emp.getProvincia());
 		
 		JLabel lblProvincia = new JLabel("Provincia");
 		lblProvincia.setBounds(603, 16, 68, 14);
@@ -188,6 +201,7 @@ public class tablaEmpleados extends JFrame {
 		   
 			cedulaJF = new JFormattedTextField(mascaraCedula);
 			cedulaJF.setBounds(16, 94, 111, 20);
+			cedulaJF.setEditable(false);
 		
 		}catch (ParseException e) {
 			e.printStackTrace();
@@ -226,45 +240,11 @@ public class tablaEmpleados extends JFrame {
 		
 		
 		//model = new DefaultTableModel();
-		model = new DefaultTableModel(
-				new Object[][] {
-				},
-				new String[] {
-					"ID", "SSN", "Cedula", "Nombre", "S. Nombre", "Apellido", "Calle", "Ciudad", "C. Postal", "Telefono", "Posicion", "Salario", "Provincia"
-				}
-			);
-			
-		tableEmpleado = new JTable();
-		tableEmpleado.setModel(model);
-		/*tableEmpleado.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"ID", "SSN", "Cedula", "Nombre", "S. Nombre", "Apellido", "Posicion", "C\u00F3digo Postal", "Provincia", "Ciudad", "Calle", "Tel\u00E9fono", "Salario"
-			}
-		));*/
-		scrollPaneEmpleado.setViewportView(tableEmpleado);
+
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(SystemColor.inactiveCaption);
 		panel_1.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addComponent(panel, GroupLayout.DEFAULT_SIZE, 978, Short.MAX_VALUE)
-				.addComponent(scrollPaneEmpleado, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 978, Short.MAX_VALUE)
-				.addComponent(panel_1, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 978, Short.MAX_VALUE)
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 135, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addComponent(scrollPaneEmpleado, GroupLayout.PREFERRED_SIZE, 327, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 58, Short.MAX_VALUE))
-		);
 		panel.setLayout(null);
 		panel.add(cedulaJF);
 		panel.add(tfPosicion);
@@ -292,13 +272,7 @@ public class tablaEmpleados extends JFrame {
 		panel.add(lblPostal);
 		panel_1.setLayout(null);
 		
-		JButton btnInsertarEmp = new JButton("REGISTRAR");
-		btnInsertarEmp.setForeground(Color.BLACK);
-		btnInsertarEmp.setBackground(Color.LIGHT_GRAY);
-		btnInsertarEmp.setBounds(761, 11, 107, 34);
-		panel_1.add(btnInsertarEmp);
-		
-		JButton btnCerrar = new JButton("CERRAR");
+		JButton btnCerrar = new JButton("CANCELAR");
 		btnCerrar.setBackground(Color.LIGHT_GRAY);
 		btnCerrar.setForeground(Color.BLACK);
 		btnCerrar.setBounds(876, 11, 96, 34);
@@ -307,84 +281,41 @@ public class tablaEmpleados extends JFrame {
 		JButton btnModificar = new JButton("MODIFICAR");
 		btnModificar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int filaTable = tableEmpleado.getSelectedRow();
-	
-					String id = model.getValueAt(filaTable, 0).toString(); //ID
-					String ssn = model.getValueAt(filaTable, 1).toString(); 
-					String cedula = model.getValueAt(filaTable, 2).toString();
-					String nombre = model.getValueAt(filaTable, 3).toString();
-					String sName = model.getValueAt(filaTable, 4).toString();
-					String apellido = model.getValueAt(filaTable, 5).toString(); 
-					String calle = model.getValueAt(filaTable, 6).toString();
-					String ciudad = model.getValueAt(filaTable, 7).toString();
-					String cPostal = model.getValueAt(filaTable, 8).toString();
-					String telefono = model.getValueAt(filaTable, 9).toString();
-					String posicion = model.getValueAt(filaTable, 10).toString();
-					Float salario = Float.parseFloat(model.getValueAt(filaTable, 11).toString() ); 
-					String provincia = model.getValueAt(filaTable, 12).toString();
+				try {
+					Empleado auxEmp = new Empleado(cedulaJF.getText(), tfNombre.getText(), tfSname.getText(), tfApellido.getText(), tfCalle.getText(),
+							tfCiudad.getText(), TelefonoJF.getText(), CodPostJF.getText(), tfSSN.getText(), tfPosicion.getText(), Float.parseFloat(tfSalario.getText()),
+							cbxProvincia.getSelectedItem().toString());
+					Bike_Rental.getInstance().updateEmpleado(auxEmp);
 					
-					Empleado auxEm = new Empleado(cedula, nombre, sName, apellido, calle, ciudad, telefono, cPostal, ssn, posicion, salario, provincia);
-					ModificarEmpleado modify = new ModificarEmpleado(auxEm);
-					modify.setVisible(true);
-					modify.addWindowListener(new WindowAdapter() {
-						public void windowClosed(WindowEvent e) {
-							try {
-								cargarEmpleados();
-							} catch (Exception e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-						}
-					});
-
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
 			}
 		});
-		btnModificar.setBounds(639, 11, 114, 34);
+		btnModificar.setBounds(772, 11, 96, 34);
 		panel_1.add(btnModificar);
 		btnCerrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
 			}
 		});
-		btnInsertarEmp.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Empleado emp1 = new Empleado(cedulaJF.getText(), tfNombre.getText(), tfSname.getText(), tfApellido.getText(), tfCalle.getText(), tfCiudad.getText(), 
-					TelefonoJF.getText(), CodPostJF.getText(), tfSSN.getText(), tfPosicion.getText(), Float.parseFloat(tfSalario.getText()), cbxProvincia.getSelectedItem().toString());
-				try {
-					Bike_Rental.getInstance().insertEmpleado(emp1);
-					cargarEmpleados();
-				} catch (Exception e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		});
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addComponent(panel, GroupLayout.PREFERRED_SIZE, 980, GroupLayout.PREFERRED_SIZE)
+				.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 980, GroupLayout.PREFERRED_SIZE)
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(10)
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 135, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE))
+		);
 		contentPane.setLayout(gl_contentPane);
 	}
-	public void cargarEmpleados() throws Exception {
-		model.setRowCount(0);
-		tableEmpleado.setModel(model);
-		String sql = "select * from Empleado";
-		try {
-			PreparedStatement ps = Bike_Rental.getInstance().conectarSQL().prepareStatement(sql);
-			ResultSet rs = ps.executeQuery();
-			//ResultSetMetaData  rsMd = rs.getMetaData();
-			//int cantcolum = rsMd.getColumnCount();
-			while (rs.next()) {
-				fila = new Object[13];
-				for (int i = 0; i<13; i++) {
-					fila[i] = rs.getString(i+1);
-				}
-				model.addRow(fila);
-
-			}
-		}catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-	}
-	public tablaEmpleados getInstance(){
-		tablaEmpleados t1 = new tablaEmpleados();
-		return t1;
+	public boolean confirmacion() {
+		return confirmar;
 	}
 }
